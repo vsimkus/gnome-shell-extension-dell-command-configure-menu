0be62bbb215175df8ffb35cbd37e6ed402cf80dc
Vaidotas Simkus <simkus.vaidotas@gmail.com>
2021-11-24 18:35:38 +0000

    Fix preference window issue on Gnome 40+

--- dell-command-configure-menu@vsimkus.github.io/prefs.js
+++ dell-command-configure-menu@vsimkus.github.io/prefs.js
@@ -26,7 +26,10 @@ function buildPrefsWidget() {
 
     // Create a parent widget that we'll return from this function
     let prefsWidget = new Gtk.Grid({
-        margin: 18,
+        margin_bottom: 18,
+        margin_end: 18,
+        margin_start: 18,
+        margin_top: 18,
         column_spacing: 12,
         row_spacing: 12,
         visible: true
