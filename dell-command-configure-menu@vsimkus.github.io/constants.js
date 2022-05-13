e8222eeeed807ebca6774a3e04b489eec6491b60
Vaidotas Simkus <simkus.vaidotas@gmail.com>
2022-05-13 11:25:54 +0100

    Gnome 42

--- dell-command-configure-menu@vsimkus.github.io/constants.js
+++ dell-command-configure-menu@vsimkus.github.io/constants.js
@@ -1,15 +1,15 @@
 // Settings
-const CUSTOM_CHARGE_START_KEY = 'custom-charge-start-charging';
-const CUSTOM_CHARGE_STOP_KEY = 'custom-charge-stop-charging';
-const ASK_BIOS_SETUP_PASSWORD_KEY = 'ask-bios-setup-password';
-const INDICATE_CURRENT_CHARGE_MODE = 'indicate-current-charge-mode';
-const CURRENT_CHARGE_MODE_KEY = 'current-charge-mode';
-const ENABLE_CUSTOM_CHARGE_KEY = 'enable-custom-charge';
-const ENABLE_STANDARD_CHARGE_KEY = 'enable-standard-charge';
-const ENABLE_ADAPTIVE_CHARGE_KEY = 'enable-adaptive-charge';
-const ENABLE_EXPRESS_CHARGE_KEY = 'enable-express-charge';
+var CUSTOM_CHARGE_START_KEY = 'custom-charge-start-charging';
+var CUSTOM_CHARGE_STOP_KEY = 'custom-charge-stop-charging';
+var ASK_BIOS_SETUP_PASSWORD_KEY = 'ask-bios-setup-password';
+var INDICATE_CURRENT_CHARGE_MODE = 'indicate-current-charge-mode';
+var CURRENT_CHARGE_MODE_KEY = 'current-charge-mode';
+var ENABLE_CUSTOM_CHARGE_KEY = 'enable-custom-charge';
+var ENABLE_STANDARD_CHARGE_KEY = 'enable-standard-charge';
+var ENABLE_ADAPTIVE_CHARGE_KEY = 'enable-adaptive-charge';
+var ENABLE_EXPRESS_CHARGE_KEY = 'enable-express-charge';
 
 // Charge constants
-const CUSTOM_CHARGE_MIN_START = 50;
-const CUSTOM_CHARGE_MAX_STOP = 100;
-const CUSTOM_CHARGE_INCREMENTS = 5;
+var CUSTOM_CHARGE_MIN_START = 50;
+var CUSTOM_CHARGE_MAX_STOP = 100;
+var CUSTOM_CHARGE_INCREMENTS = 5;
